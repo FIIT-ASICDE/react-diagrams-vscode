b1eae213bc5b60b7f233558a5ec5cbe2a49a1611
Programmer001 <petko.hupka@gmail.com>
2026-04-11 22:36:10 +0200

    Speeding up webview watch

--- packages/webview-ui/vite.config.ts
+++ packages/webview-ui/vite.config.ts
@@ -1,35 +1,45 @@
 import { defineConfig } from 'vite'
 import path from 'node:path'
 import react from '@vitejs/plugin-react'
+import reactSwc from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-    tailwindcss(),
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  const enableReactCompiler = mode === 'compiler'
+  const fast = mode === 'fast'
+  const reactPlugin = enableReactCompiler ? react({
+    babel: {
+      plugins: [['babel-plugin-react-compiler']],
     },
-  },
-  build: {
-    outDir: path.resolve(__dirname, '../vscode-extension/dist/webview'),
-    // minify: false,
-    // cssMinify: false,
-    reportCompressedSize: false,
-    emptyOutDir: false,
-    rollupOptions: {
-      output: {
-        entryFileNames: `assets/[name].js`,
-        chunkFileNames: `assets/[name].js`,
-        assetFileNames: `assets/[name].[ext]`,
+  }) : reactSwc()
+
+  return {
+    plugins: [
+      reactPlugin,
+      tailwindcss(),
+      // ClosePlugin()
+    ],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
       },
     },
-  },
+    build: {
+      outDir: path.resolve(__dirname, '../vscode-extension/dist/webview'),
+      target: 'esnext',
+      minify: enableReactCompiler,
+      cssMinify: enableReactCompiler,
+      reportCompressedSize: enableReactCompiler,
+      emptyOutDir: false,
+      rollupOptions: {
+        treeshake: fast ? false : undefined,
+        output: {
+          entryFileNames: `assets/[name].js`,
+          chunkFileNames: `assets/[name].js`,
+          assetFileNames: `assets/[name].[ext]`,
+        },
+      },
+    },
+  }
 })
